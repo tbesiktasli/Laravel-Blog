9db9eb37feeb1e9af3523560a6701fe5600e4a95
tbesiktasli <tolga.besiktasli@gmail.com>
2014-11-20 22:00:27 +0100

    updated sluggable code to version 2
    - added sluggabletrait
    - added sluggableinterface
    - changed visibility of config array to protected

--- src/models/Post.php
+++ src/models/Post.php
@@ -1,10 +1,13 @@
 <?php namespace Fbf\LaravelBlog;
 
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class Post extends \Eloquent {
+class Post extends \Eloquent implements SluggableInterface {
 
 	use SoftDeletingTrait;
+	use SluggableTrait;
 	protected $dates = ['deleted_at'];
 	
 	/**
@@ -32,7 +35,7 @@ class Post extends \Eloquent {
 	 * Used for Cviebrock/EloquentSluggable
 	 * @var array
 	 */
-	public static $sluggable = array(
+	protected $sluggable = array(
 		'build_from' => 'title',
 		'save_to' => 'slug',
 		'separator' => '-',
